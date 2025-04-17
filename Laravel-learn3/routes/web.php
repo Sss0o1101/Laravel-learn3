7d401918d31a615c7e63e997c8e5e207b71fbbfc
alias <matusoyo2436@gmail.com>
2025-04-17 19:43:00 +0900

    new text

--- Laravel-learn3/routes/web.php
+++ Laravel-learn3/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts2', [PostController::class, 'index2'])->name('posts.index2');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
