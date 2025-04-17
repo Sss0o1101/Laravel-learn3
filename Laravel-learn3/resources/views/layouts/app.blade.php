7cdecdbf9acc862650679b6dc20451cc1941cdd7
alias <matusoyo2436@gmail.com>
2025-04-17 19:43:20 +0900

    new file

---
+++ Laravel-learn3/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header style="background-color: #f8f8f8; padding: 20px 0;">
+        <h1>インスタグラム風アプリ</h1>
+    </header>
+    <div class="content">
+        @yield('content')
+    </div>
+    <footer>
+        <p>© 2025 Instagram Clone</p>
+    </footer>
+</body>
+</html>
