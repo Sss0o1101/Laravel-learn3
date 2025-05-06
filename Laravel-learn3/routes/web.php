c60f5e49f9aab1a5ad2de5a114c6e5d153e5b2de
alias <matusoyo2436@gmail.com>
2025-05-06 18:32:41 +0900

    ルート

--- Laravel-learn3/routes/web.php
+++ Laravel-learn3/routes/web.php
@@ -2,10 +2,24 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// 投稿一覧
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+// 投稿作成
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+// 投稿保存
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+
+
 
 
 
