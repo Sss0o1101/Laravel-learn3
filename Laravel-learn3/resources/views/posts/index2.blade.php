3fb2f15ca1c8171870a1446ed370c1c9c06b200e
alias <matusoyo2436@gmail.com>
2025-05-13 22:17:58 +0900

    viws

--- Laravel-learn3/resources/views/posts/index2.blade.php
+++ Laravel-learn3/resources/views/posts/index2.blade.php
@@ -3,11 +3,17 @@
 @section('title', '投稿一覧')
 
 @section('content')
-<h1>2の投稿一覧</h1>
+<x-alert type="warning">
+    これは警告メッセージです。
+</x-alert>
+<h1 class="text-center">2の投稿一覧</h1>
 @foreach ($posts as $post)
-    <div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <h2>タイトル：{{ $post->title }}</h2>
         <p>本文：{{ $post->body }}</p>
+        <button class="bg-blue-800 text-white px-4 py-2 rounded">
+            投稿する
+        </button>
     </div>
 @endforeach
 @endsection
